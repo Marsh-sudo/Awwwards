1e26304903a87d26e8ad1f6d01c27eaabe6dade7
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-09 19:10:05 +0300

    create function for search

--- awards/views.py
+++ awards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from .models import Post,Profile,Rating
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .forms import PostForm
+from .forms import PostForm,UpdateUserProfileForm
 
 
 # Create your views here.
@@ -27,4 +27,29 @@ def new_post(request):
         post_form = PostForm()
 
     return render(request,'all-awards/new_post.html',{"post_form":post_form})
+
+
+def profile(request):
+    profile = Profile.display_profile()
+    current_user = request.user
+    if request.method == "POST":
+        profile_form = UpdateUserProfileForm(request.POST,request.FILES)
+        if profile_form.is_valid():
+            profile_form.save()
+    
+    else:
+        profile_form = UpdateUserProfileForm()
+
+    return render (request,'all-awards/profile.html',{"profile_form":profile_form,"profiles":profile})
+
+
+def search_post(request):
+    if request.method == 'GET':
+        title = request.GET.get("title")
+        searched_posts = Post.objects.filter(title__icontains=title).all()
+
+    return render (request,'all-awards/search.html',{"projects":searched_posts})
+
+
+
 
