eed0a2498d3936f0d32ca538f155c9edb75e3b0f
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-11 01:10:51 +0300

    create path for api

--- awards/urls.py
+++ awards/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     re_path('^$',views.index,name="home"),
     re_path('^new/post$',views.new_post,name='new-post'),
-    re_path('^profile$',views.profile,name='profile'),
-    re_path('^search$',views.search_post,name='search')
+    re_path('^profiles$',views.profile,name='profile'),
+    re_path('^search$',views.search_post,name='search'),
+    re_path('^api/award/$',views.AwardList.as_view())
 ]
