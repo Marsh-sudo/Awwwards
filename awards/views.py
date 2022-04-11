bc269b55713e2cebebe3e00e4be2812b3fb2427e
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-11 15:42:19 +0300

    create view function

--- awards/views.py
+++ awards/views.py
@@ -11,21 +11,31 @@ from rest_framework.response import Response
 
 
 # Create your views here.
-# @login_required(login_url='/accounts/register/')
+@login_required(login_url='/accounts/register/')
 def index(request):
     posts = Post.objects.all()
     all_users = User.objects.exclude(id=request.user.id)
     current_user = request.user
-    return render(request,'all-awards/home.html',{"posts":posts,"all_users":all_users,"current_user":current_user})
+    items = Post.display()
+
+
+    return render(request,'all-awards/home.html',{"posts":posts,"all_users":all_users,"current_user":current_user,"items":items})
 
 def new_post(request):
     current_user = request.user
     if request.method == "POST":
         post_form = PostForm(request.POST,request.FILES)
+        post_form.instance.user = request.user
+        print(post_form)
         if post_form.is_valid():
-            post = post_form.save()
-            post = current_user
-            post.save()
+            # post = Post()
+            # post.title = post_form.cleaned_data['title']
+            # post.description = post_form.cleaned_data['description']
+            # post.link = post_form.cleaned_data['link']
+            # post.image = post_form.cleaned_data['image']
+            # post.user = post_form.cleaned_data['user']
+            # post.save()
+            post_form.save()
 
             return redirect('home')
     else:
@@ -47,36 +57,6 @@ def profile(request):
 
     return render (request,'all-awards/profile.html',{"profiles_form":profile_form,"profiles":profile})
 
-
-# def ratings(request):
-#     post = Post.objects.all()
-#     ratings = Rating.objects.filter()
-#     if request.method == 'POST':
-#         form = RatingForm(request.Post)
-#         if form.is_valid():
-#             rate_result = form.save()
-#             rate_result.save()
-#             post_ratings = Rating.objects.filter(post=post)
-
-#             design_rate = [d.design for d in post_ratings]
-#             design_av = sum(design_rate) / len(design_rate)
-
-#             usability_rate = [us.usability for us in post_ratings]
-#             usability_av  = sum(usability_rate) / len(usability_rate)
-
-#             content_rate = [cont.content for cont in post_ratings]
-#             content_av = sum(content_rate) / len(content_rate)
-
-#             total = (design_av + usability_av + content_av) / 3
-#             rate_result.design_average = round(design_av, 2)
-#             rate_result.usability_average = round(usability_av, 2)
-#             rate_result.content_average = round(content_av, 2)
-#             rate_result.total = round(total, 2)
-#             rate_result.save()
-#             return HttpResponseRedirect(request.path_info)
-#         else:
-#             form = RatingForm()
-#         return render(request,'all-awards/rating.html',{"post":post,"ratings":ratings,"rate_form":form})    
 
 
 def search_post(request):
