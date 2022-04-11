bc64852aa694f6e9ab82f1db4c2ef37730d9f181
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-11 15:43:31 +0300

    add media

--- tribune/settings.py
+++ tribune/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+
 
 STATIC_URL = 'static/'
 
@@ -127,6 +135,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
