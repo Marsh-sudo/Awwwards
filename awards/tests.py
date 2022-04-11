19e886dacafe6c0c99fe0a2bdea731cede9f8a11
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-12 01:16:43 +0300

    create tests

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post,Rating
 
 # Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user = User(username='mary',email="mary@gmail.com", password='password')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+class PostTest(TestCase):
+
+       def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
